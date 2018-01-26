f6c259304075d973e9c269f4292349fde45eea69
gkaragoz <gkaragoz96@gmail.com>
2018-01-26 22:34:21 +0300

    Add Interact Range gizmo

--- Revive/Assets/Scripts/PlayerController.cs
+++ Revive/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
 
+    public float interactRange = 2f;
+    [HideInInspector]
     public Transform target;
 
     private NavMeshAgent agent;
@@ -22,5 +25,14 @@ public class PlayerController : MonoBehaviour {
     void GetReferences() {
         agent = GetComponent<NavMeshAgent>();
         target = GameObject.Find("Target").transform;
+    }
+
+    void OnDrawGizmosSelected() {
+        Handles.color = new Color(1, 0, 0, 0.1f);
+        Handles.DrawSolidArc(transform.position,
+                transform.up,
+                -transform.right,
+                360,
+                interactRange);
     }
 }
