d38a44dd9bee7249782eb8811fe6f3e156f230c9
gkaragoz <gkaragoz96@gmail.com>
2018-01-26 22:20:37 +0300

    Create PlayerController script.

---
+++ Revive/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Transform target;
+
+    private NavMeshAgent agent;
+
+    void Start () {
+        GetReferences();
+    }
+
+    void FixedUpdate() {
+        if (target != null) {
+            agent.destination = target.position;
+        }
+    }
+
+    void GetReferences() {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Target").transform;
+    }
+}
