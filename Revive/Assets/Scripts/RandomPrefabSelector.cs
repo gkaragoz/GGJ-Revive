a0232c692f0f24512e5270482aeeb669c5d97451
gkaragoz <gkaragoz96@gmail.com>
2018-01-27 23:40:49 +0300

    Improve level design

--- Revive/Assets/Scripts/RandomPrefabSelector.cs
+++ Revive/Assets/Scripts/RandomPrefabSelector.cs
@@ -8,10 +8,13 @@ public class RandomPrefabSelector : MonoBehaviour {
     public List<GameObject> prefabs = new List<GameObject>();
 
     void Awake() {
-        int randomIndex = Random.Range(0, prefabs.Count);
-        GameObject obj = Instantiate(prefabs[randomIndex], Vector3.zero, Quaternion.identity);
-        obj.transform.parent = this.transform;
-        obj.transform.position = Vector3.zero;
-        obj.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        if (transform.childCount <= 1)
+        {
+            int randomIndex = Random.Range(0, prefabs.Count);
+            GameObject obj = Instantiate(prefabs[randomIndex], Vector3.zero, Quaternion.identity);
+            obj.transform.parent = this.transform;
+            obj.transform.position = Vector3.zero;
+            obj.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        }
     }
 }
