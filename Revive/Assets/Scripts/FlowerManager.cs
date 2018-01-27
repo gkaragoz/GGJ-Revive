e442a7b5334ccccda72a8478d4c234d995b8168a
gkaragoz <gkaragoz96@gmail.com>
2018-01-27 03:49:43 +0300

    Fix bug on Flower turnings

--- Revive/Assets/Scripts/FlowerManager.cs
+++ Revive/Assets/Scripts/FlowerManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class FlowerManager : MonoBehaviour {
 
+    public bool isTurning = false;
     public bool isGreen = true;
     public Material darkFlower;
     public Material greenFlower;
@@ -16,29 +17,37 @@ public class FlowerManager : MonoBehaviour {
     }
 
     public void OnInteracted() {
-        if (isGreen == true) {
-            BecomeDark();
-        } else {
-            GrowGreen();
+        if (isTurning == false) {
+            if (isGreen == true) {
+                BecomeDark();
+            } else {
+                GrowGreen();
+            }
         }
     }
 
     void BecomeDark() {
+        isTurning = true;
         LeanTween.scale(this.gameObject, Vector3.zero, 2f).setEaseOutQuad().setOnComplete(OnBecomeDarkComplete);
     }
 
     void OnBecomeDarkComplete() {
-        LeanTween.scale(this.gameObject, Vector3.one, 2f).setEaseOutQuad();
+        LeanTween.scale(this.gameObject, Vector3.one, 2f).setEaseOutQuad().setOnComplete(OnFinish);
         flower.material = darkFlower;
         isGreen = false;
     }
 
+    void OnFinish() {
+        isTurning = false;
+    }
+
     void GrowGreen() {
+        isTurning = true;
         LeanTween.scale(this.gameObject, Vector3.zero, 2f).setEaseOutQuad().setOnComplete(OnGrowGreenComplete);
     }
 
     void OnGrowGreenComplete() {
-        LeanTween.scale(this.gameObject, Vector3.one, 2f).setEaseOutQuad();
+        LeanTween.scale(this.gameObject, Vector3.one, 2f).setEaseOutQuad().setOnComplete(OnFinish);
         flower.material = greenFlower;
         isGreen = true;
     }
