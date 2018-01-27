1ecca5a792e90949eb82492e36e69f98d1902b18
gkaragoz <gkaragoz96@gmail.com>
2018-01-27 22:14:26 +0300

    Add randomPrefabSelector.cs

---
+++ Revive/Assets/Scripts/RandomPrefabSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RandomPrefabSelector : MonoBehaviour {
+
+    public List<GameObject> prefabs = new List<GameObject>();
+
+    void Awake() {
+        int randomIndex = Random.Range(0, prefabs.Count);
+        GameObject obj = Instantiate(prefabs[randomIndex], Vector3.zero, Quaternion.identity);
+        obj.transform.parent = this.transform;
+        obj.transform.position = Vector3.zero;
+        obj.transform.localRotation = Quaternion.Euler(Vector3.zero);
+    }
+}
