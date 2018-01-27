78df8557ae72d3144fca74644bd56c6ffe7775a4
gkaragoz <gkaragoz96@gmail.com>
2018-01-27 21:57:40 +0300

    Add RandomSizer script works in RuntimeEditor

---
+++ Revive/Assets/Scripts/RandomSizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RandomSizer : MonoBehaviour {
+
+    public Vector3 size = Vector3.one;
+
+    public float maxSize = 1f;
+    public float minSize = 0.1f;
+
+    void Awake() {
+        size = Vector3.one * Random.Range(minSize, maxSize);
+        transform.localScale = size;
+    }
+}
