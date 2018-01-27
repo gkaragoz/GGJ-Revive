269776ffcfd1cbe919e8120d68ad04ec3a0a2408
gkaragoz <gkaragoz96@gmail.com>
2018-01-28 01:34:27 +0300

    Import bgMusic gameplay

---
+++ Revive/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance;
+
+    private AudioSource source;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+
+        source = GetComponent<AudioSource>();
+
+        DontDestroyOnLoad(this);
+    }
+
+}
