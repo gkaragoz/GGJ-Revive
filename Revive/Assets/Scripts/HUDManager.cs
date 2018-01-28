afb7129ae5da7625dfeff90ad254c773a4cba2a2
huseyinyasar <huseyinyassar@gmail.com>
2018-01-28 15:37:04 +0300

    Create HUD

---
+++ Revive/Assets/Scripts/HUDManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour {
+	public Slider playerHealthSlider;
+	public Slider opponentHealthSlider;
+	public GameObject player;
+	public GameObject opponent;
+
+	void Awake(){
+		playerHealthSlider.value = 100;
+		opponentHealthSlider.value = 100;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		playerHealthSlider.value = player.GetComponent<PlayerController> ().currentHealth / player.GetComponent<PlayerController> ().maxHealth;
+		opponentHealthSlider.value = opponent.GetComponent<OpponentAI> ().currentHealth / opponent.GetComponent<OpponentAI> ().maxHealth;
+
+	}
+}
