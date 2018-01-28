18472c9358ad5d464852e7f687531dc466d6aaf8
huseyinyasar <huseyinyassar@gmail.com>
2018-01-28 11:47:29 +0300

    Add cursor and target crosshair fbx.

---
+++ Revive/Assets/TargetAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LeanTween.scale(this.gameObject, transform.localScale * 1.5f, 0.5f).setEase(LeanTweenType.pingPong).setLoopType(LeanTweenType.pingPong);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
